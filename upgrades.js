20d2d15e0a21c2bac980869ca5080cc4a2614e87
DeletedUser-0 <71786022+DeletedUser-0@users.noreply.github.com>
2021-04-01 20:08:36 +0000

    Update upgrades.js

--- upgrades.js
+++ upgrades.js
@@ -1,8 +1,8 @@
 function upgrade1() {
     if (Decimal.compare(game.points.total, game.upgrade1.cost) >= 0) {
         game.points.total = Decimal.sub(game.points.total, game.upgrade1.cost);
-        game.upgrade1.cost = Decimal.times(game.upgrade1.cost, 1.0625);
-        game.points.perTick = Decimal.times(game.points.perTick, 1.05);
+        game.upgrade1.cost = Decimal.times(game.upgrade1.cost, 1.125);
+        game.points.upgradebonus = Decimal.times(game.points.upgradebonus, 1.1);
         game.upgrade1.level = Decimal.add(game.upgrade1.level, 1);
     };
 };
@@ -11,7 +11,14 @@ function upgrade2() {
     if (Decimal.compare(game.points.total, game.upgrade2.cost) >= 0) {
         game.points.total = Decimal.sub(game.points.total, game.upgrade2.cost);
         game.upgrade2.cost = Decimal.times(game.upgrade2.cost, 10);
-        game.points.perTick = Decimal.times(game.points.perTick, 1.333333333333333333);
+        game.points.upgradebonus = Decimal.times(game.points.upgradebonus, 1.333333333333333333);
         game.upgrade2.level = Decimal.add(game.upgrade2.level, 1);
     }
 }
+
+function upgrade3() {
+    if (Decimal.compare(game.Ppoints.total, game.PrestigeUpgrade1.cost) >= 0) {
+        game.PrestigeUpgrade1.cost = Decimal.times(game.PrestigeUpgrade1.cost, 2);
+        game.PrestigeUpgrade1.level = Decimal.add(game.PrestigeUpgrade1.level, 1);
+    };
+};

d67a376992bb2e0d57eee64db2281c3adc21ed3d
DeletedUser-0 <71786022+DeletedUser-0@users.noreply.github.com>
2021-04-15 18:58:26 +0100

    Update main.js

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ var game = {
 
 function addPoints() {
     game.points.total = Decimal.add(game.points.perTick, game.points.total);
-    game.Ppoints.earn =  Decimal.pow(10, game.points.total.exponent/9 - 0.7)
+    game.Ppoints.earn =  Decimal.pow(10, game.points.total.exponent/45 - 0.7)
     game.points.perTick = Decimal.times(0.02, game.PrestigeUpgrade1.effectiveness).times(game.points.upgradebonus).times(game.PrestigeUpgrade2.effectiveness).times(game.PrestigeUpgrade3.effectiveness);
     game.time = Decimal.add(0.02, game.time);
     game.Ppoints.time = Decimal.add(0.02, game.Ppoints.time);

498800cdacca0e03e5e5fe8817e9ddbe6455182a
DeletedUser-0 <71786022+DeletedUser-0@users.noreply.github.com>
2021-04-23 19:33:19 +0100

    Update dev.js

--- JavaScript/dev.js
+++ JavaScript/dev.js
@@ -1,8 +1,8 @@
 function points() {
-    game.points.total = Decimal.exp(game.points.total, 2.5);
+    game.points.total = OmegaNum.exp(game.points.total, 2.5);
 };
 
 function Ppoints() {
-    game.Ppoints.total = Decimal.pow(game.Ppoints.total, 1.25).times(3).add(1);
+    game.Ppoints.total = OmegaNum.pow(game.Ppoints.total, 1.25).times(3).add(1);
     game.Ppoints.reset += 1;
 };

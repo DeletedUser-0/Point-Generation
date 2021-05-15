a8b00caefec9070a7da71381ab7428351bc35ef1
DeletedUser-0 <71786022+DeletedUser-0@users.noreply.github.com>
2021-05-15 14:48:20 +0100

    Update settings.js

--- JavaScript/settings.js
+++ JavaScript/settings.js
@@ -1,13 +1,9 @@
 function showPPS() {
-    if (game.settings.showPerTick === "true") {
-        game.settings.showPerTick === "false";
-    } else {
-        game.settings.showPerTick === "true";
-    }
-}
-
-if (game.settings.showPerTick === "true") {
-    document.querySelector("PPS").style.display = "block";
-} else {
-    document.querySelector("PPS").style.display = "none";
-}
+    game.settings.showPerTick ^= true;
+    document.querySelector('#PPS').style.display = (game.settings.showPerTick ? 'block' : 'none');
+    document.querySelector('#tab1').style.bottom = (game.settings.showPerTick ? '0cm' : '1.25cm');
+    document.querySelector('#tab2').style.bottom = (game.settings.showPerTick ? '0cm' : '1.25cm');
+    document.querySelector('#tab3').style.bottom = (game.settings.showPerTick ? '0cm' : '1.25cm');
+    document.querySelector('#tab4').style.bottom = (game.settings.showPerTick ? '0cm' : '1.25cm');
+    document.querySelector('#tab5').style.bottom = (game.settings.showPerTick ? '0cm' : '1.25cm');
+};
